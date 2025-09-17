bb16438d5d063f687910af3fc58477ccd20d46c1
Tasneem Kurabadwala <Tasneem.Kurabadwala@softwire.com>
2025-09-17 14:23:29 +0100

    Make data display more readable. Add error handling for empty text.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ function App() {
   };
 
   async function handleSubmit() {
+    if(text === "") {
+      setArrivalsData("No postcode entered! Please enter a postcode first.");
+      return;
+    }
     const response = await getArrivalsGivenPostCode(text);
     console.log(response);
     if(response === null) {
@@ -22,30 +26,32 @@ function App() {
 
   return (
         <>
-        <h1 className="text-3xl font-bold underline text-center text-cyan-600 m-4"
+        <h1 className="text-3xl font-bold text-center text-cyan-600 m-8"
         >BusBoard</h1>
-        <input
-          className="m-12"
-          type="text"
-          value={text}
-          placeholder="Type here..."
-          onChange={handleChange}
-        />
-        <button className="underline text-cyan-600" onClick={handleSubmit}>Submit</button>
-        <div className="m-12">
+        <div className="text-center border-black">
+          <input
+            className="p-2 m-12 text-center border-black"
+            type="text"
+            value={text}
+            placeholder="Type here..."
+            onChange={handleChange}
+          />
+          <button className="underline text-white px-4 py-2 bg-cyan-600 rounded" onClick={handleSubmit}>Search</button>
+        </div>
+        <div className="m-12 text-center">
           {arrivalsData === undefined && (
-            <p>Please enter a postcode and click Submit.</p>
+            <p>Please enter a postcode and click Search.</p>
           ) }
           {typeof arrivalsData === 'string' && (
             <p className="text-red-500">{arrivalsData}</p>
           )}
           {Array.isArray(arrivalsData) && arrivalsData.length > 0 && arrivalsData.map((row, rowIndex) => (
-              <div key={rowIndex}>Bus Stop {rowIndex+1}
-                <ul>
+              <div key={rowIndex} className="m-12 border-2 border-black rounded-xl"><p className="text-xl text-cyan-700 underline">{row[0].stationName}</p>
+                <ol className="list-decimal list-inside mx-auto text-left pl-4 w-fit">
                   {row.length>0 && row.map((item, colIndex) => (
-                    <li key={colIndex}>Stop: {item.stationName},    Route: {item.towards},     Destination: {item.destinationName},     Time Until Arrival: {item.timeToStationMinutes} </li>
+                    <li key={colIndex}>{item.destinationName} via {item.towards}, {item.timeToStationMinutes} </li>
                   ))}
-                </ul>
+                </ol>
               </div>
           ))}
         </div>

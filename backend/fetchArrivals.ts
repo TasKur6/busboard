7a69a303cc6b2099864df0d6802acbd663adc601
Tasneem Kurabadwala <Tasneem.Kurabadwala@softwire.com>
2025-09-18 09:59:40 +0100

    Markup: remove comments as code itself is readable enough.

--- backend/fetchArrivals.ts
+++ backend/fetchArrivals.ts
@@ -19,21 +19,17 @@ export async function getArrivalsGivenPostCode(postCode: string): Promise<Arriva
     const stopPoints = await getStopPointsGivenLatLong(latLong);
     if(stopPoints === null) return "Bus Stops not found";
 
-    //Sort bus stops by nearest to farthest
     stopPoints.sort((a, b) => a.distance - b.distance);
 
-    //Get the two nearest bus stops
     const nearestTwoStops = stopPoints.slice(0,2);
 
-    //Get bus arrival information for each of the bus stops
     const arrivalsPotential = await Promise.all(
         nearestTwoStops.map(async (item) => {
         const tempArrivals = await getArrivalsGivenStopPoint(item.naptanId);
-        return tempArrivals ?? []; // return empty array if null
+        return tempArrivals ?? [];
         })
     );
 
-    //Checking for empty arrays in case of null return from API call
     const arrivals: ArrivalInfo[][] = [];
     for(const point of arrivalsPotential) {
         if (point.length === 0) {
@@ -49,17 +45,12 @@ async function getArrivalsGivenStopPoint(stopPoint: string): Promise<ArrivalInfo
   try {
     const response = await axios.get(`https://api.tfl.gov.uk/StopPoint/${stopPoint}/Arrivals?app_key=${api_key}`);
 
-    //Sort bus arrivals by earliest to latest
     response.data.sort((a: { timeToStation: number; }, b: { timeToStation: number; }) => a.timeToStation - b.timeToStation);
 
-    //Get the first five buses at given stop
-    const firstFiveRows = response.data.slice(0,5);
+    const firstFiveBuses = response.data.slice(0,5);
 
-    //Return as ArrivalInfo type
-    return firstFiveRows.map((item: any): ArrivalInfo => {
+    return firstFiveBuses.map((item: any): ArrivalInfo => {
         const {stationName, lineName, destinationName, timeToStation, towards} = item;
-
-        //timeToStation gives a value whose unit is seconds, so convert this to a readable string of the form 'Xm Ys'
         const timeToStationMinutes = `${Math.floor(timeToStation/60)}m ${timeToStation%60}s`;
         return {stationName, lineName, destinationName, timeToStationMinutes, towards};
     });
@@ -74,10 +65,8 @@ async function getStopPointsGivenLatLong(latLong: number[]): Promise<StopPoint[]
         const stopType = "NaptanPublicBusCoachTram";
         const latLongResponse = await axios.get(`https://api.tfl.gov.uk/StopPoint/?lat=${latLong[0]}&lon=${latLong[1]}&stopTypes=${stopType}&app_key=${api_key}`);
 
-        //Get stopPoints list from total JSON data
         const {stopPoints} = latLongResponse.data;
 
-        //Return as stopPoint type
         return stopPoints.map((item: any): StopPoint => {
             const {naptanId, distance} = item;
             return {naptanId, distance};
@@ -92,10 +81,8 @@ async function getLatLongGivenPostCode(postCode: string): Promise<number[] | nul
     try {
         const postCodeResponse = await axios.get(`https://api.postcodes.io/postcodes/${postCode}`);
 
-        //Get latitude and longitude from total JSON data
         const {latitude, longitude} = postCodeResponse.data.result;
 
-        //Return as a number array
         const latLong: number[] = [];
         latLong[0] = latitude;
         latLong[1] = longitude;
